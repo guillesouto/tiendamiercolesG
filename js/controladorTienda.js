70801a18363399a3f105c8dc036394fbc4335528
203 <203@academicos.cesde.edu.co>
2022-02-16 10:07:32 -0500

    se establecion un controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,31 @@
+// ENTRADA DEL PROBLEMA (comentario de linea)
+/*
+comentario 
+de
+bloque
+*/
+
+//ENTRADAS==VARIABLES==CONSTANTES
+let nombreProducto='mascara'
+let precioProducto=80000
+let pesoProducto=2.4
+let tieneDescuento=true
+let descripcionProducto='Mascara de spiderman clasico en distintos tamaños, no incluye faceshield'
+let telefonoVendedor="3226516658"
+
+const NOMBRE_CLIENTE="Juan José"
+
+//ARREGLOS UNIDIMENCIONALES (vectores)
+//Sirve para almacenar varios elementos en 1 sola variable
+let productos=['mascara', 'funco', 'disfraz', 'camisa', 'videojuego']
+let productos2=Array('mascara', 'funco', 'disfraz', 'camisa', 'videojuego')
+
+//SALIDAS
+console.log("buenas")
+console.log(nombreProducto)
+console.log("su nombre es: "+NOMBRE_CLIENTE+"y su producto e:s "+nombreProducto)
+console.log(`su nombre es: ${NOMBRE_CLIENTE} y su producto en ${nombreProducto}`)
+console.log('*******************************************************************')
+console.log(productos)
+console.log(productos2[0])
+
