e63dcf50af27f1e53e3b16bc996ddd0d4cfe8c52
203 <203@academicos.cesde.edu.co>
2022-02-23 08:54:18 -0500

    se crean ejemplos DOM

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -3,8 +3,6 @@
 comentario 
 de
 bloque
-*/
-
 //ENTRADAS==VARIABLES==CONSTANTES
 let nombreProducto='mascara'
 let precioProducto=80000
@@ -28,4 +26,26 @@ console.log(`su nombre es: ${NOMBRE_CLIENTE} y su producto en ${nombreProducto}`
 console.log('*******************************************************************')
 console.log(productos)
 console.log(productos2[0])
+*/
+
+//controlando etiquetas
+
+//1. crear una variable para guardar etiqueT 
+let etiquetaTitulo = document.getElementById("titulo")
+console.log(etiquetaTitulo)
+
+//2 COMO MODIFICAR ETIQUETAs
+etiquetaTitulo.textContent="rojo vos no existis"
+
+//prueba NavBar
+let etiquetaNavbar = document.getElementById("icono")
+etiquetaNavbar.textContent="Spider-Ham"
+
+//editar foto
+let etiquetaFoto = document.getElementById("fotospider")
+etiquetaFoto.src="img/dT9rbg8ac.png"
+
+//cambiar el estilo de la etiqueta
+etiquetaTitulo.classList.add("text-center")
+etiquetaTitulo.classList.add("text-danger")
 
